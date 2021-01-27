0f449dfa1c25e56283bea4f587033fdeafb11c3a
Anwesh43 <anweshthecool0@gmail.com>
2021-01-28 03:58:14 +0530

    adding LDBNode class

--- index.ts
+++ index.ts
@@ -135,3 +135,45 @@ class Animator {
         }
     }
 }
+
+class LDBNode {
+
+    prev : LDBNode 
+    next : LDBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LDBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLDBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LDBNode {
+        var curr : LDBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
