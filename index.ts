a2085732e609fdf2419503d5018b616fdb681457
Anwesh43 <anweshthecool0@gmail.com>
2021-01-28 03:33:48 +0530

    adding ScaleUtil class

--- index.ts
+++ index.ts
@@ -14,3 +14,12 @@ const colors : Array<string> = [
     "#0D47A1"
 ]
 
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
